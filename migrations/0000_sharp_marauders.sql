c3883ef83a6d51a75eab7239ed4a0448e4fd54cc
Peter <s@pramos.me>
2024-09-13 10:06:58 +0200

    feat: half drizzle

---
+++ migrations/0000_sharp_marauders.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `session` (
+	`id` text PRIMARY KEY NOT NULL,
+	`user_id` text NOT NULL,
+	`expires_at` integer NOT NULL,
+	`token` text NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `user` (
+	`id` text PRIMARY KEY NOT NULL,
+	`spotify_id` text NOT NULL,
+	`email` text NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `user_spotify_id_unique` ON `user` (`spotify_id`);--> statement-breakpoint
+CREATE UNIQUE INDEX `user_email_unique` ON `user` (`email`);
