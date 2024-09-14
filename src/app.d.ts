bb86ab1de32fe77bcf35be04c13a74a27a20361c
Peter <s@pramos.me>
2024-09-14 03:33:11 +0200

    chore: try other things

--- src/app.d.ts
+++ src/app.d.ts
@@ -7,15 +7,6 @@ declare global {
       user: import("lucia").User | null;
       session: import("lucia").Session | null;
     }
-    interface Platform {
-      env: {
-        COUNTER: DurableObjectNamespace;
-      };
-      context: {
-        waitUntil(promise: Promise<any>): void;
-      };
-      caches: CacheStorage & { default: Cache }
-    }
     // interface PageData {}
     // interface PageState {}
     // interface Platform {}
